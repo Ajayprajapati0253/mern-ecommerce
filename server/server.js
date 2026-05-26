9423883ac14e9b2763fa79a663de8aa254db6adb
Ajayprajapati0253 <ajayprajapati0253@gamil.com>
2026-05-26 16:29:33 +0530

    mongodb connected Successfully

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/",(req,res) => {
+    res.send("API running successfully");
+});
+
+// Port
+const PORT = process.env.PORT || 5000;
+
+// mongodb connection
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("Mongodb connected");
+
+        app.listen(PORT, () => {
+            console.log(`server running on port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log(error);
+    });
