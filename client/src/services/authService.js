69fff8cfa4d1eb410db1971970e53ed0b98d7e8a
Ajayprajapati0253 <ajayprajapati0253@gamil.com>
2026-05-27 11:31:43 +0530

    Updated production backend URLs

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/api/auth";
+// const API = "http://localhost:5000/api/auth";
+const API = "https://video-learning-backend.onrender.com";
 
 
 // REGISTER USER
--- client/src/services/videoService.js
+++ client/src/services/videoService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/api/videos";
+// const API = "http://localhost:5000/api/videos";
+const API = "https://video-learning-backend.onrender.com";
 
 
 // Get Token
